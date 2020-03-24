afa176edc30aa2b9ec0d33c1ee34b72cb215f779
William Friconneau <4367901+WiwiWillou@users.noreply.github.com>
2020-03-24 20:51:00 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ from mqtt_client import MQTT_Hassio
 from tydom_websocket import TydomWebSocketClient
 
 
-#TODO : an options.json and others adjustements to make a hassio addons, as a branch
-
 ####### CREDENTIALS TYDOM
 TYDOM_MAC = os.getenv('TYDOM_MAC') #MAC Address of Tydom Box
 TYDOM_IP = os.getenv('TYDOM_IP', 'mediation.tydom.com') # Local ip address, default to mediation.tydom.com for remote connexion if not specified
@@ -20,10 +18,6 @@ TYDOM_PASSWORD = os.getenv('TYDOM_PASSWORD') #Tydom password
 MQTT_HOST = os.getenv('MQTT_HOST', 'localhost')
 MQTT_USER = os.getenv('MQTT_USER')
 MQTT_PASSWORD = os.getenv('MQTT_PASSWORD')
-
-####### OPTIONS
-# SYS_CONTEXT = os.getenv('SYS_CONTEXT', None) # None if you don't use systemd, preparing for docker....
-
 
 loop = asyncio.get_event_loop()
 
@@ -53,11 +47,6 @@ def loop_task():
     hassio.tydom = tydom
 
     print('Start websocket listener and heartbeat')
-    # if SYS_CONTEXT == 'systemd':
-    #     import sdnotify
-    #     # Inform systemd that we've finished our startup sequence...
-    #     n = sdnotify.SystemdNotifier()
-    #     n.notify("READY=1")
 
     tasks = [
         tydom.receiveMessage(),
