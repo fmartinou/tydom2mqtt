f3fdcc42947d21fea14b56ff9286b374f24bb657
William Friconneau <4367901+mrwiwi@users.noreply.github.com>
2020-04-05 23:55:25 +0200

    Add files via upload

--- forever.py
+++ forever.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-#!/usr/bin/python
+
 from subprocess import Popen
 import sys
 
 filename = sys.argv[1]
 while True:
     print("\nStarting " + filename)
-    p = Popen("python " + filename, shell=True)
+    p = Popen("python3 " + filename, shell=True)
     p.wait()
 
 
