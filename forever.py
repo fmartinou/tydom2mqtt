b473044e0190f7c7e14bfc25a2637320569f5230
William Friconneau <4367901+mrwiwi@users.noreply.github.com>
2020-04-05 23:36:18 +0200

    Changes for hassio true addon compatibility

--- forever.py
+++ forever.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+#!/usr/bin/python
 from subprocess import Popen
 import sys
 
+filename = sys.argv[1]
 while True:
-    print("\nStarting forever script....")
-    p = Popen("python3 -u main.py", shell=True)
+    print("\nStarting " + filename)
+    p = Popen("python " + filename, shell=True)
     p.wait()
+
+
+    #thanks https://ep.gnt.md/index.php/how-to-restart-python-script-after-exception-and-run-it-forever/
