6a847d140f69e6b3d06e2d80b24fe9827328f905
CyrilP <5814027+CyrilP@users.noreply.github.com>
2021-12-16 19:56:55 +0100

    fix sensor type for windows giving status : OPEN, CLOSE, OPEN_HOPPER

--- app/sensors.py
+++ app/sensors.py
@@ -45,7 +45,7 @@ class sensor:
         self.binary = False
         # self.device_class = None
         self.config_topic = sensor_config_topic.format(id=self.id)
-        if 'unit_of_measurement' not in tydom_attributes_payload.keys() and (self.elem_value == False or self.elem_value):
+        if 'unit_of_measurement' not in tydom_attributes_payload.keys() and (self.elem_value in ["0", "1", "true", "false"] or type(self.elem_value) == bool):
             self.binary = True
             self.json_attributes_topic = binary_sensor_json_attributes_topic.format(
                 id=self.id)
